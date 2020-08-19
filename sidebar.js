edae2dad7cf29cab77380e047882dee404697744
h-karim <karimh96@hotmail.com>
2020-08-19 14:57:32 -0400

    Final report draft

---
+++ sidebar.js
@@ -0,0 +1,18 @@
+document.write(`
+						<div class="flex-container" id="side">
+								<div class="header">
+										<h1>My Website</h1>
+										<p>by Karim Hasbini.</p>
+								</div>
+								<div  >
+										<p>Hi, I'm Karim Hasbini. I'm a software engineering student at <a href="https://www.concordia.ca/">ConcordiaU</a>. 
+										<br/><a href="#">More about me</a>.
+										</p>
+
+								</div>
+								<div id="contact">
+										<h3>Contact </h3>
+										You can find me on <a href="https://www.github.com/h-karim">GitHub</a> and <a href="https://www.linkedin.com/in/karim-hasbini-231758125/">LinkedIn</a>
+								</div>
+		</div>
+		`);
