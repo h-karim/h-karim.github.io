d8253c8cbb8b5b31050437afa249c930dbd8bd97
h-karim <karimh96@hotmail.com>
2020-10-01 18:55:00 -0400

    Add sidebar photo

---
+++ pages/sidebar.js
@@ -0,0 +1,18 @@
+document.write(`
+						<div class="flex-container" id="side">
+								<div class="header">
+										<h1>Karim Hasbini</h1>
+								</div>
+								<div  >
+										<img src='../assets/pictures/me_woods_cropped.jpg'>
+										<p>Hi, I'm Karim. I'm a software engineering student at <a href="https://www.concordia.ca/">ConcordiaU</a>. 
+										<br/><a href="#">More about me</a>.
+										</p>
+
+										<div id="contact">
+												<h3>Contact </h3>
+												You can find me on <a href="https://www.github.com/h-karim">GitHub</a> and <a href="https://www.linkedin.com/in/karim-hasbini-231758125/">LinkedIn.</a>
+										</div>
+								</div>
+		</div>
+		`);
