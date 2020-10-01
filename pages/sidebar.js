d4c90c3f67fa5553e9423a7f488eb9949eb5e658
h-karim <karimh96@hotmail.com>
2020-10-01 19:24:52 -0400

    Check file to load image

--- pages/sidebar.js
+++ pages/sidebar.js
@@ -1,10 +1,14 @@
+var filename = window.location.pathname.split('/').pop()
+imagePath = (filename == 'index.html' ? 'assets/pictures/me_woods_cropped.jpg' : '../assets/pictures/me_woods_cropped.jpg');
+
+
 document.write(`
 						<div class="flex-container" id="side">
 								<div class="header">
 										<h1>Karim Hasbini</h1>
 								</div>
 								<div  >
-										<img src='../assets/pictures/me_woods_cropped.jpg'>
+										<img src=${imagePath}>
 										<p>Hi, I'm Karim. I'm a software engineering student at <a href="https://www.concordia.ca/">ConcordiaU</a>. 
 										<br/><a href="#">More about me</a>.
 										</p>
