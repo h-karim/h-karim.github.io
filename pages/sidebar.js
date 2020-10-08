d06491eea7d6c3e1e16a437966ec2cb9f40d2435
h-karim <karimh96@hotmail.com>
2020-10-08 17:46:34 -0400

    Add logos

--- pages/sidebar.js
+++ pages/sidebar.js
@@ -1,5 +1,5 @@
 var filename = window.location.pathname.split('/').pop()
-imagePath = (filename == 'index.html' ? 'assets/pictures/me_woods_cropped.jpg' : '../assets/pictures/me_woods_cropped.jpg');
+imagePath = (filename == 'index.html' ? 'assets/pictures/' : '../assets/pictures/');
 
 
 document.write(`
@@ -8,14 +8,19 @@ document.write(`
 										<h1>Karim Hasbini</h1>
 								</div>
 								<div  >
-										<img src=${imagePath}>
+										<img id='profile-pic' src=${imagePath}me_woods_cropped.jpg>
 										<p>Hi, I'm Karim. I'm a software engineering student at <a href="https://www.concordia.ca/">ConcordiaU</a>. 
 										<br/><a href="#">More about me</a>.
 										</p>
 
 										<div id="contact">
 												<h3>Contact </h3>
-												You can find me on <a href="https://www.github.com/h-karim">GitHub</a> and <a href="https://www.linkedin.com/in/karim-hasbini-231758125/">LinkedIn.</a>
+												<a href="https://www.github.com/h-karim">
+														<img id='github' src=${imagePath}GitHub-Mark-64px.png alt="GitHub">
+												</a>
+												<a href="https://www.linkedin.com/in/karim-hasbini-231758125/">
+														<img id='linkedin' src=${imagePath}LI-In-Bug.png>
+												</a>
 										</div>
 								</div>
 		</div>
