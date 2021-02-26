cf453c8f950a688bb0d2a71002d4e34451b42e18
h-karim <karimh96@hotmail.com>
2021-02-25 20:02:07 -0500

    implement partial loading

--- assets/js/jQDriver.js
+++ assets/js/jQDriver.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
 		$('#side').load('/assets/sidebar.html');
 		$('#navbar').load('/assets/navbar.html');
+		$('#container').load('content.html');
+		$(document).on('click','a.directionless', function(e){
+				e.preventDefault();
+				let href = $(this).attr('href');
+				$('#container').load(href + ' #content');
+		});
+
 });
